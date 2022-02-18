c36a4f7dd4ce097a03e9856d375c00ad109be1d5
acnow <acvarela77@gmail.com>
2022-02-17 18:21:23 -0700

    hashrouter added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
